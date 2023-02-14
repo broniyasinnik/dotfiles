03831ee350c7770c66bcfc6478396af3f733f8a5
Roni Yasinnik <roniy@playtika.com>
2023-02-14 15:47:52 +0200

    Adding .zshrc

---
+++ install_plugins.sh
@@ -0,0 +1,12 @@
+sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
+
+# Install zsh autosuggestions
+# git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+# Install fast syntax-highlighting
+git clone https://github.com/zdharma-continuum/fast-syntax-highlighting ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/fast-syntax-highlighting
+
+# Install auto jump
+git clone git://github.com/wting/autojump.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/autojump
+cd ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/autojump
+./install.py
